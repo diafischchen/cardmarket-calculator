0adf16d11291706d0d9ac2d63de4a379e55c28ae
diafischchen <68379878+diafischchen@users.noreply.github.com>
2023-04-19 19:01:23 +0200

    Example Data

---
+++ dataexample.php
@@ -0,0 +1,42 @@
+<?php
+
+[
+    'theme' => 'dark',
+    'version' => 1,
+    'cards' => [
+        [
+            'name' => 'Trasla (SVI 211)',
+            'link' => 'https://www.cardmarket.com/de/Pokemon/Products/Singles/Scarlet-Violet/Ralts-V2-SV1en211',
+            'image' => 'https://static.cardmarket.com/img/a525653463c48700b553f50f05db6560/items/51/SVI/702507.jpg',
+            'prices' => [
+                [
+                    'price' => '12,87 €',
+                    'captured' => '2023-04-19',
+                    'category' => 'price-trend'
+                ],
+                [
+                    'price' => '13,61 €',
+                    'captured' => '2023-04-18',
+                    'category' => 'price-trend'
+                ],
+            ],
+        ],
+        [
+            'name' => 'Forschung des Professors (SVI 240)',
+            'link' => 'https://www.cardmarket.com/de/Pokemon/Products/Singles/Scarlet-Violet/Professors-Research-V3-SV1en240',
+            'image' => 'https://static.cardmarket.com/img/a215d9bce78d50d1a9f8db839ce56de5/items/51/SVI/702536.jpg',
+            'prices' => [
+                [
+                    'price' => '7,61 €',
+                    'captured' => '2023-04-19',
+                    'category' => 'price-trend'
+                ],
+                [
+                    'price' => '10,03 €',
+                    'captured' => '2023-04-19',
+                    'category' => '30-day-trend'
+                ]
+            ]
+        ]
+    ]
+];
